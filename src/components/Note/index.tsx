581e49077c0c13b8f8fe60a1d7dd363d0af8124b
Ganesh Adapa <36997100+succulentpup@users.noreply.github.com>
2021-06-08 00:20:44 +0100

    feat: Saving the note to DB using mock endpoint. Now when the user logs in, it will load the note with the value fetched from DB.

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
 import { useAppSelector } from "../../app/hooks";
 import { selectAuth, LoginStatus } from "../Login/authslice";
 
 export function Note() {
+  const [timer, setTimer] = useState(0);
   const auth = useAppSelector(selectAuth);
 
   if (auth.status !== LoginStatus.LOGGED_IN) return null;
   const {
     apiToken,
-    user: { id: userId },
+    user: { id: userId, note: existingNote },
   } = auth;
+
+  const createTimer = (body: { id: string; note: string }, waitTime: number) =>
+    setTimeout(async () => {
+      await fetch(
+        `https://60b793ec17d1dc0017b8a6bc.mockapi.io/users/${ userId }`,
+        {
+          method: "PUT",
+          headers: { "content-type": "application/json", "Authorization": apiToken },
+          body: JSON.stringify(body),
+        }
+      );
+    }, waitTime);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (timer === 0) {
+      const timerId = createTimer({ id: userId, note: event.target.value}, 2000)
+      setTimer(timerId as unknown as number);
+    } else{
+      clearTimeout(timer);
+      const timerId = createTimer({ id: userId, note: event.target.value}, 2000)
+      setTimer(timerId as unknown as number);
+    }
+  };
 
   return (
     <div>
-      <NoteField />
+      <input type="text" onChange={handleOnChange} defaultValue={existingNote}/>
     </div>
   );
-}
-
-function NoteField() {
-  return <textarea defaultValue="Note goes here..."></textarea>;
 }
